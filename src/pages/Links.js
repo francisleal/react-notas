b610f38afe1b546cdb04640055852f59b5ebf61a
fancisleal <francis.leal@outlook.com>
2019-07-24 00:22:52 -0300

    criacao lista estatica

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 
 class Links extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            links: [
+                {
+                    id: 1,
+                    link: 'https://www.w3schools.com/',
+                    tipo: 'importante'
+                },
+                {
+                    id: 2,
+                    link: 'lorem ipsum sit amet 1',
+                    tipo: 'e-mail'
+                },
+                {
+                    id: 3,
+                    link: 'lorem ipsum sit amet 2',
+                    tipo: 'importante'
+                }
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="card">
@@ -9,114 +32,34 @@ class Links extends Component {
                 </div>
                 <div className="card-body">
                     <div className="row">
-                        <div className="col-md-3">
+                        <div className="col-md-12">
                             <table className="table table-bordered">
                                 <thead>
                                     <tr>
-                                        <th>ALM</th>
-                                        <th>action</th>
+                                        <th>Link</th>
+                                        <th>Tipo</th>
+                                        <th style={{textAlign:'right'}}>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>https://marvelapp.com/</td>
-                                        <td>
-                                            <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-save'></i></button>
-                                            <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-pen'></i></button>
-                                            <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-trash'></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Mary</td>
-                                        <td>
-                                            <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-pen'></i></button>
-                                            <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-trash'></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>July</td>
-                                        <td>Dooley</td>
-                                    </tr>
+                                    {this.state.links.map((link, index) => {
+                                        console.log('link ',link);
+                                        return (
+                                            <tr key={link.id}>
+                                                <td><a href={link.link} rel='noreferrer noopener' target='_blank'>{link.link}</a></td>
+                                                <td>{link.tipo}</td>
+                                                <td style={{textAlign:'right'}}>
+                                                    <button type="button" className="btn btn-primary btn-sm btn-success"><i className='fas fa-save'></i></button>
+                                                    <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-pen'></i></button>
+                                                    <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-trash'></i></button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
                                 </tbody>
                             </table>
-                        </div>
-                        <div className="col-md-3">
-                            <table className="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>Links</th>
-                                        <th>action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            {/* <button type="button" className="btn btn-primary btn-sm btn-success"><i className='fas fa-save'></i></button> */}
-                                            <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-pen'></i></button>
-                                            <button type="button" className="btn btn-primary btn-sm btn-light"><i className='fas fa-trash'></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Mary</td>
-                                        <td>Moe</td>
-                                    </tr>
-                                    <tr>
-                                        <td>July</td>
-                                        <td>Dooley</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div className="col-md-3">
-                            <table className="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>E-mails</th>
-                                        <th>action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>John</td>
-                                        <td>Doe</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Mary</td>
-                                        <td>Moe</td>
-                                    </tr>
-                                    <tr>
-                                        <td>July</td>
-                                        <td>Dooley</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div className="col-md-3">
-                            <table className="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>Importantes</th>
-                                        <th>action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>John</td>
-                                        <td>Doe</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Mary</td>
-                                        <td>Moe</td>
-                                    </tr>
-                                    <tr>
-                                        <td>July</td>
-                                        <td>Dooley</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        </div>                     
                     </div>
-
                 </div>
             </div>
         );
